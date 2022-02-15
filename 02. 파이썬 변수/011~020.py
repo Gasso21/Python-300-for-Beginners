4cb6a3b77c1e146436e9b63d650ed2f6e49c4fa1
Jaehwan <jhjung1227@naver.com>
2022-02-15 22:00:36 +0900

    '22.02.15.

---
+++ 02. 파이썬 변수/011~020.py
@@ -0,0 +1,42 @@
+#011
+s = 50000
+print(s*10)
+
+#012
+s = '298조'
+h = '50,000원'
+PER = '15.79'
+
+#013
+s = "hello"
+t = "python"
+print(s+"!", t)
+
+#014
+print(2 + 2 * 3)
+
+#015
+a = "132"
+print(type(a))
+
+#016
+num_str = "720"
+num_str = int(num_str)
+print(num_str)
+
+#017
+num = 100
+num = str(num)
+
+#018
+print(type(float("15.79")))
+print(float("15.79"))
+
+#019
+year = "2020"
+year = int(year)
+print(year, year+1, year+2)
+
+#020
+air = 48584
+print(air*36)
