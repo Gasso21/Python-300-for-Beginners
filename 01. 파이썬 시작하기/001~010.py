adf57efed53a6e7bcb326b877b3f9ea7b8d27492
Jaehwan <jhjung1227@naver.com>
2022-02-15 21:11:13 +0900

    '22.02.15.

--- 01. 파이썬 시작하기/001~010.py
+++ 01. 파이썬 시작하기/001~010.py
@@ -3,6 +3,7 @@ print('Hello World')
 
 #002
 print('Mary\'s cosmetics')
+print("Mary's cosmetics")
 
 #003
 print('신씨가 소리질렀다. "도둑이야".')
@@ -15,3 +16,18 @@ print("안녕하세요.\n만나서\t\t반갑습니다.")
 
 #006
 print ("오늘은", "일요일")
+
+#007
+print("naver;kakao;sk;samsung")
+print("naver", "kakao", "sk", "samsung", sep=";")
+
+#008
+print("naver/kakao/sk/samsung")
+print("naver", "kakao", "sk", "samsung", sep="/")
+
+#009
+print("first", end=" ");print("second")
+
+#010
+print("%s" %(5/3))
+print(5/3)
