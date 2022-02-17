e27c9e5cc60ad143ba86d3e11e2c9d2f5073d217
Jaehwan <jhjung1227@naver.com>
2022-02-17 21:00:54 +0900

    '22.02.17.

---
+++ 03. 파이썬 문자열/031~040.py
@@ -0,0 +1,49 @@
+#031
+a = "3"
+b = "4"
+print(a + b)
+
+#032
+print("Hi" * 3)
+
+#033
+print('-' * 80)
+
+#034
+t1 = 'python'
+t2 = 'java'
+print((t1 + ' ' + t2 + ' ') * 4)
+
+#035
+name1 = "김민수"
+age1 = 10
+name2 = "이철희"
+age2 = 13
+print('이름: %s 나이: %d' %(name1, age1));print('이름: %s 나이: %d' %(name2, age2))
+
+#036
+name1 = "김민수"
+age1 = 10
+name2 = "이철희"
+age2 = 13
+print("이름: {} 나이: {}".format(name1, age1))
+print("이름: {} 나이: {}".format(name2, age2))
+
+#037
+name1 = "김민수"
+age1 = 10
+name2 = "이철희"
+age2 = 13
+print(f"이름: {name1} 나이: {age1}")
+print(f"이름: {name2} 나이: {age2}")
+
+#038
+s = "5,969,782,550"
+print(int(s.replace(',','')))
+
+#039
+h = "2020/03(E) (IFRS연결)"
+print(h[:10])
+
+#040
+data = "   삼성전자    "
