70e9897d044a8eb4b869109c5f99fb4d54f098e6
Jaehwan <jhjung1227@naver.com>
2022-02-20 22:24:43 +0900

    '22.02.20.

---
+++ 03. 파이썬 문자열/041~050.py
@@ -0,0 +1,43 @@
+#041
+ticker = "btc_krw"
+print(ticker.upper())
+
+#042
+ticker = "BTC_KRW"
+print(ticker.lower())
+
+#043
+print('hello'.capitalize())
+
+#044
+file_name = "보고서.xlsx"
+print(file_name.endswith('.xlsx'))
+
+#045
+file_name = "보고서.xlsx"
+print(file_name.endswith(('xlsx','xls')))
+"""
+file_name = "보고서.xlsx"
+print(file_name.endswith("xlsx", "xls"))
+>>> TypeError: slice indices must be integers or None or have an __index__ method
+"""
+
+#046
+file_name = "2020_보고서.xlsx"
+print(file_name.startswith('2020'))
+
+#047
+a = "hello world"
+print(a.split())
+
+#048
+ticker = "btc_krw"
+print(ticker.split('_'))
+
+#049
+date = "2020-05-01"
+print(date.split('-'))
+
+#050
+data = "039490     "
+print(data.rstrip())
