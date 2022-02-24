8dd88ab26dab7773a0baa08d1338d0fa7743c139
Jaehwan <jhjung1227@naver.com>
2022-02-25 00:11:25 +0900

    '22.02.24.

--- 07. 파이썬 분기문/121~130.py
+++ 07. 파이썬 분기문/121~130.py
@@ -7,8 +7,31 @@ else:
 print(x)
 
 #122
+x = int(input("score: "))
+print("grade is ", end="")
+if 81<=x<=100:
+    print("A")
+elif 61<=x<=80:
+    print("B")
+elif 41<=x<=60:
+    print("C")
+elif 21<=x<=40:
+    print("D")
+elif 0<=x<=20:
+    print("E")
 
 #123
+x = input("입력: ")
+x = x.split(" ")
+if x[1] == "달러":
+    result = 1167 * int(x[0])
+elif x[1] == "엔":
+    result = 1.096 * int(x[0])
+elif x[1] == "유로":
+    result = 1268 * int(x[0])
+elif x[1] == "위안":
+    result = 171 * int(x[0])
+print("%0.2d 원" %result)
 
 #124
 
