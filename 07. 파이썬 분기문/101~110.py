34654b7c7dfce870a97bd1dc70c4f5044436ff0e
Jaehwan <jhjung1227@naver.com>
2022-02-24 21:33:32 +0900

    '22.02.22.

--- 07. 파이썬 분기문/101~110.py
+++ 07. 파이썬 분기문/101~110.py
@@ -1,20 +1,49 @@
 #101
-
+"""
+bool
+"""
+print(type(True))
 
 #102
+True
 
 #103
+True
 
 #104
+True
 
 #105
+True
 
 #106
+print(3>=4)
 
 #107
+if 4 < 3:
+    print("Hello World")
 
 #108
+if 4 < 3:
+    print("Hello World.")
+else:
+    print("Hi, there.")
 
 #109
+if True :
+    print ("1")
+    print ("2")
+else :
+    print("3")
+print("4")
 
 #110
+if True :
+    if False:
+        print("1")
+        print("2")
+    else:
+        print("3")
+else :
+    print("4")
+print("5")
