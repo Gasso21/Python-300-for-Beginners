b44030bee9da5056189208e46fc0db1c0bf0b698
Jaehwan <jhjung1227@naver.com>
2022-03-07 21:37:11 +0900

    '22.03.07.

---
+++ 08. 파이썬 반복문/131~140.py
@@ -0,0 +1,64 @@
+#131
+과일 = ["사과", "귤", "수박"]
+for 변수 in 과일:
+    print(변수)
+
+#132
+과일 = ["사과", "귤", "수박"]
+for 변수 in 과일:
+  print("#####")
+
+#133
+for 변수 in ["A", "B", "C"]:
+  print(변수)
+
+#134
+for 변수 in ["A", "B", "C"]:
+  print("출력:", 변수)
+
+#135
+for 변수 in ["A", "B", "C"]:
+  b = 변수.lower()
+  print("변환:", b)
+
+#136
+변수 = 10
+print(변수)
+변수 = 20
+print(변수)
+변수 = 30
+print(변수)
+
+for i in ['10', '20', '30']:
+    print(i)
+
+#137
+print(10)
+print(20)
+print(30)
+for i in [10, 20, 30]:
+    print(i)
+
+#138
+print(10)
+print("-------")
+print(20)
+print("-------")
+print(30)
+print("-------")
+for i in [10,20,30]:
+    print(i, "\n-------")
+
+#139
+print("++++")
+print(10)
+print(20)
+print(30)
+
+print("++++")
+for i in [10, 20, 30]:
+    print(i)
+
+#140
+for i in range(4):
+    print("-------")
