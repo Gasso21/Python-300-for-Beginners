ba6ce534fa36affe6ceeb13d9e87822e369fd666
Jaehwan <jhjung1227@naver.com>
2022-03-08 23:17:52 +0900

    '22.03.08.

--- 08. 파이썬 반복문/171~180.py
+++ 08. 파이썬 반복문/171~180.py
@@ -1,10 +1,36 @@
 #171
+price_list = [32100, 32150, 32000, 32500]
+for i in range(4):
+    print(price_list[i])
+
 #172
+price_list = [32100, 32150, 32000, 32500]
+for i in range(4):
+    print(i, price_list[i])
+
 #173
+price_list = [32100, 32150, 32000, 32500]
+for i in range(4):
+    print(3-i, price_list[i])
+
 #174
+price_list = [32100, 32150, 32000, 32500]
+for i in range(1,4):
+    print(90+10*i, price_list[i])
+
 #175
+my_list = ["가", "나", "다", "라"]
+for i in range(3):
+    print(my_list[i], my_list[i+1])
+
 #176
+my_list = ["가", "나", "다", "라", "마"]
+for i in range(3):
+    print(my_list[i], my_list[i+1], my_list[i+2])
+
 #177
+
+
 #178
 #179
 #180
