53c7d8c75a9d64e827f4f0829e91bb0284e86c66
Jaehwan <jhjung1227@naver.com>
2022-03-09 12:06:07 +0900

    '22.03.09.

--- 08. 파이썬 반복문/171~180.py
+++ 08. 파이썬 반복문/171~180.py
@@ -33,15 +33,30 @@ my_list = ["가", "나", "다", "라"]
 for i in range(3,0,-1):
     print(my_list[i], my_list[i-1])
 
+"""
+for i in range(len(my_list) - 1, 0, -1):
+    print(my_list[i], my_list[i-1])
+"""
+
 #178
 my_list = [100, 200, 400, 800]
 for i in range(3):
     print(my_list[i+1] - my_list[i])
 
+"""
+for i in range(len(my_list) - 1):
+    print(abs(my_list[i+1] - my_list[i]))
+"""
+
 #179
 my_list = [100, 200, 400, 800, 1000, 1300]
 for i in range(4):
     print((my_list[i]+my_list[i+1]+my_list[i+2])/3)
+
+"""
+for i in range(1, len(my_list) - 1):
+    print(abs(my_list[i-1] + my_list[i] + my_list[i+1]) / 3)
+"""
 
 #180
 low_prices  = [100, 200, 400, 800, 1000]
