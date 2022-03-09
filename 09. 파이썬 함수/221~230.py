35abffd199a9ae95d3d44ae88dc03a94fa6dc6b4
Jaehwan <jhjung1227@naver.com>
2022-03-09 20:29:50 +0900

    '22.03.09.

--- 09. 파이썬 함수/221~230.py
+++ 09. 파이썬 함수/221~230.py
@@ -1,10 +1,40 @@
 #221
+def print_reverse(string):
+    print(string[::-1])
+print_reverse("python")
+
 #222
+def print_score(a):
+    sum = 0
+    for i in a:
+        sum += i
+    result = sum/len(a)
+    print(result)
+print_score([1, 2, 3])
+
 #223
+def print_even(l):
+    for i in l:
+        if i % 2 == 0:
+            print(i)
+print_even ([1, 3, 2, 10, 12, 11, 15])
+
 #224
+def print_keys(d):
+    for keys in d.keys():
+        print(keys)
+print_keys ({"이름":"김말똥", "나이":30, "성별":0})
+
 #225
+my_dict = {"10/26" : [100, 130, 100, 100],
+           "10/27" : [10, 12, 10, 11]}
+
 #226
+
 #227
+
 #228
+
 #229
+
 #230
