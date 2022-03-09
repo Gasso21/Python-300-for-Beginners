290d1df2b111473b935d2cb3c0699ef09440563d
Jaehwan <jhjung1227@naver.com>
2022-03-09 21:15:50 +0900

    '22.03.09.

--- 09. 파이썬 함수/221~230.py
+++ 09. 파이썬 함수/221~230.py
@@ -28,13 +28,42 @@ print_keys ({"이름":"김말똥", "나이":30, "성별":0})
 #225
 my_dict = {"10/26" : [100, 130, 100, 100],
            "10/27" : [10, 12, 10, 11]}
+def print_value_by_key(dict, key):
+    print(dict[key])
+
+print_value_by_key  (my_dict, "10/26")
 
 #226
+def print_5xn(string):
+    x = len(string) // 5
+    for i in range(x+1):
+        print(string[5*i:5*(i+1)])
+
+print_5xn("아이엠어보이유알어걸")
 
 #227
+def printmxn(string, number):
+    x = len(string) // number
+    for i in range(x+1):
+        print(string[number*i:number*(i+1)])
+
+printmxn("아이엠어보이유알어걸", 3)
 
 #228
+def calc_monthly_salary(n):
+    print(n//12)
+calc_monthly_salary(12000000)
 
 #229
+def my_print (a, b) :
+    print("왼쪽:", a)
+    print("오른쪽:", b)
+
+my_print(a=100, b=200)
 
 #230
+def my_print (a, b) :
+    print("왼쪽:", a)
+    print("오른쪽:", b)
+
+my_print(b=100, a=200)
