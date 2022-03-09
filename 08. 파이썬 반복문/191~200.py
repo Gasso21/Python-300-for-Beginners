c3b8529511363b94a01a781c0d01aa4cded038d1
Jaehwan <jhjung1227@naver.com>
2022-03-09 15:27:57 +0900

    '22.03.09.

--- 08. 파이썬 반복문/191~200.py
+++ 08. 파이썬 반복문/191~200.py
@@ -1,10 +1,88 @@
 #191
+data = [
+    [ 2000,  3050,  2050,  1980],
+    [ 7500,  2050,  2050,  1980],
+    [15450, 15050, 15550, 14900]
+]
+for i in data:
+    for j in i:
+        print(j*1.00014)
+
 #192
+for i in data:
+    for j in i:
+        print(j*1.00014)
+    print("----")
+
 #193
+result = []
+for i in data:
+    for j in i:
+        result.append(j*1.00014)
+print(result)
+
 #194
+result = []
+tmp = []
+
+for i in data:
+    for j in i:
+        tmp.append(j*1.00014)
+    result.append(tmp)
+    tmp = []
+print(result)
+
 #195
+ohlc = [["open", "high", "low", "close"],
+        [100, 110, 70, 100],
+        [200, 210, 180, 190],
+        [300, 310, 300, 310]]
+for i in ohlc[1:]:
+    print(i[3])
+
 #196
+ohlc = [["open", "high", "low", "close"],
+        [100, 110, 70, 100],
+        [200, 210, 180, 190],
+        [300, 310, 300, 310]]
+for i in ohlc[1:]:
+    if i[3] > 150:
+        print(i[3])
+
 #197
+ohlc = [["open", "high", "low", "close"],
+        [100, 110, 70, 100],
+        [200, 210, 180, 190],
+        [300, 310, 300, 310]]
+for i in ohlc[1:]:
+    if i[3] >= i[0]:
+        print(i[3])
+
 #198
+ohlc = [["open", "high", "low", "close"],
+        [100, 110, 70, 100],
+        [200, 210, 180, 190],
+        [300, 310, 300, 310]]
+volatility = []
+for i in ohlc[1:]:
+    volatility.append(abs(i[1] - i[2]))
+print(volatility)
+
 #199
+ohlc = [["open", "high", "low", "close"],
+        [100, 110, 70, 100],
+        [200, 210, 180, 190],
+        [300, 310, 300, 310]]
+for i in ohlc[1:]:
+    if i[3] > i[0]:
+        print(abs(i[1]-i[2]))
+
 #200
+ohlc = [["open", "high", "low", "close"],
+        [100, 110, 70, 100],
+        [200, 210, 180, 190],
+        [300, 310, 300, 310]]
+result = 0
+for i in ohlc[1:]:
+    result += i[3]-i[0]
+print(result)
