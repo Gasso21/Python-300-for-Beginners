d495b5d136d4e818e7aa7004068f389e72fc9a82
Jaehwan <jhjung1227@naver.com>
2022-03-09 22:52:18 +0900

    '22.03.09.

--- 09. 파이썬 함수/231~240.py
+++ 09. 파이썬 함수/231~240.py
@@ -27,18 +27,70 @@ l = make_list("abcd")
 print(l)
 
 #234
-
-pickup_even([3, 4, 5, 6, 7, 8])
-
+def pickup_even(l):
+    list = []
+    for i in l:
+        if i % 2 ==0:
+            list.append(i)
+    return list
+x = pickup_even([3, 4, 5, 6, 7, 8])
+print(x)
 
 #235
+def convert_int(str):
+    str = str.replace(",","")
+    return int(str)
+result = convert_int("1,234,567")
+print(result)
 
 #236
+def 함수(num) :
+    return num + 4
+
+a = 함수(10)
+b = 함수(a)
+c = 함수(b)
+print(c)
 
 #237
+def 함수(num) :
+    return num + 4
+
+c = 함수(함수(함수(10)))
+print(c)
 
 #238
+def 함수1(num) :
+    return num + 4
+
+def 함수2(num) :
+    return num * 10
+
+a = 함수1(10)
+c = 함수2(a)
+print(c)
 
 #239
+def 함수1(num) :
+    return num + 4
+
+def 함수2(num) :
+    num = num + 2
+    return 함수1(num)
+
+c = 함수2(10)
+print(c)
 
 #240
+def 함수0(num) :
+    return num * 2
+
+def 함수1(num) :
+    return 함수0(num + 2)
+
+def 함수2(num) :
+    num = num + 10
+    return 함수1(num)
+
+c = 함수2(2)
+print(c)
