c8648d05f31b024d374bd907fa1d96a350211fb7
Jaehwan <jhjung1227@naver.com>
2022-03-09 21:46:36 +0900

    '22.03.09.

--- 09. 파이썬 함수/231~240.py
+++ 09. 파이썬 함수/231~240.py
@@ -1,10 +1,42 @@
 #231
+"""
+def n_plus_1 (n) :
+    result = n + 1
+
+n_plus_1(3)
+print (result)
+"""
+"""
+함수 내부에서 사용한 변수는 함수 밖에서는 접근이 불가능
+함수 내부에서 계산한 값을 전달하기 위해서는 return을 사용
+"""
+
 #232
+def make_url(a):
+    url = "www."+a+".com"
+    return url
+
+make_url("naver")
+
 #233
+def make_list(string):
+    l = []
+    l = list(string)
+    return l
+
+make_list("abcd")
+print(l)
+
 #234
+
 #235
+
 #236
+
 #237
+
 #238
+
 #239
+
 #240
