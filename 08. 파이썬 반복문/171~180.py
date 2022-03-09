6501c2cb67dd9a9bff0c8e4a0739763e5ad1b964
Jaehwan <jhjung1227@naver.com>
2022-03-09 12:03:25 +0900

    '22.03.09.

--- 08. 파이썬 반복문/171~180.py
+++ 08. 파이썬 반복문/171~180.py
@@ -29,8 +29,25 @@ for i in range(3):
     print(my_list[i], my_list[i+1], my_list[i+2])
 
 #177
-
+my_list = ["가", "나", "다", "라"]
+for i in range(3,0,-1):
+    print(my_list[i], my_list[i-1])
 
 #178
+my_list = [100, 200, 400, 800]
+for i in range(3):
+    print(my_list[i+1] - my_list[i])
+
 #179
+my_list = [100, 200, 400, 800, 1000, 1300]
+for i in range(4):
+    print((my_list[i]+my_list[i+1]+my_list[i+2])/3)
+
 #180
+low_prices  = [100, 200, 400, 800, 1000]
+high_prices = [150, 300, 430, 880, 1000]
+volatility = []
+for i in range(5):
+    x = high_prices[i] - low_prices[i]
+    volatility.append(x)
+print(volatility)
