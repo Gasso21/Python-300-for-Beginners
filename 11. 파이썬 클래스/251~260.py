32ac988c4f975c5c3591d2cc9a4997e12844098c
Jaehwan <jhjung1227@naver.com>
2022-03-16 21:44:58 +0900

    '22.03.10.

--- 11. 파이썬 클래스/251~260.py
+++ 11. 파이썬 클래스/251~260.py
@@ -45,8 +45,51 @@ areum = Human("아름", 25, "여자")
 areum.who()
 
 #258
+class Human:
+    def __init__(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+
+    def who(self):
+        print("이름: %s, 나이: %s, 성별: %s" %(self.name, self.age, self.sex))
+
+    def setInfo(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+
 areum = Human("모름", 0, "모름")
+areum.who()
 areum.setInfo("아름", 25, "여자")
+areum.who()
+
 #259
+class Human:
+    def __init__(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+
+    def __del__(self):
+        print("나의 죽음을 알리지마라")
+
+    def who(self):
+        print("이름: {} 나이: {} 성별: {}".format(self.name, self.age, self.sex))
+
+    def setInfo(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+
+areum = Human("아름", 25, "여자")
+del areum
 
 #260
+class OMG :
+    def print() :
+        print("Oh my god")
+
+
+mystock = OMG()
+mystock.print()      # OMG.print(mystock)
